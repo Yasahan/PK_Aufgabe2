2e412227ad2683ea195b9075a452d0390ab5e3bb
Yasahan Zengin <a1367563@univie.ac.at>
2019-11-02 12:31:05 +0100

    Initial commit

---
+++ src/TrafficControllerSimple.java
@@ -0,0 +1,23 @@
+public class TrafficControllerSimple implements TrafficController{
+
+
+    @Override
+    public void enterRight(Vehicle v) {
+
+    }
+
+    @Override
+    public void enterLeft(Vehicle v) {
+
+    }
+
+    @Override
+    public void leaveLeft(Vehicle v) {
+
+    }
+
+    @Override
+    public void leaveRight(Vehicle v) {
+
+    }
+}
